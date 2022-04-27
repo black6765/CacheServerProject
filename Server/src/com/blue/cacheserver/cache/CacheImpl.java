f1212d75ba41a797106da766047313db71f1e389
black6765 <black6765@gmail.com>
2022-04-27 10:25:43 +0900

    modify: LinkedList를 이용한 Queue 방식으로 life-time 구현
    put 연산 시 queue라는 이름의 LinkedList를 이용하여 queue의 맨 마지막 인덱스에 원소를 삽입
    get 연산 시 해당 key 값의 인덱스를 조회하여 그 인덱스를 제거함과 동시에 리턴 값을 다시 큐의 마지막에 삽입하여 life-time 갱신
    remove 연산 시 queue에서 해당 key 값을 삭제
    => eviction에서 가장 오랫동안 참조되지 않은 원소를 제거하는 LRU 알고리즘 방식 적용

--- Server/src/com/blue/cacheserver/cache/CacheImpl.java
+++ Server/src/com/blue/cacheserver/cache/CacheImpl.java
@@ -15,59 +15,52 @@ public class CacheImpl<K, V> implements Cache<K, V>{
     private Map<K, V> cacheMemory = new ConcurrentHashMap<>();
 
     // delimiter인 "=<-->=" 문자열을 기준으로 앞 부분은 타임스탬프, 뒷 부분은 cacheMemory의 Key 값이 저장
-    private TreeSet<String> timeStampKeySet = new TreeSet<>(new Comparator<String>() {
-        @Override
-        public int compare(String o1, String o2) {
-            // "=<-->=" 문자열은 타임스탬프와 키를 구분하기 위한 delimiter
-            String[] str1 = o1.split("=<-->=");
-            String[] str2 = o2.split("=<-->=");
+    private List<K> queue = new LinkedList<>();
 
-            if (str1.length >= 2 && str2.length >= 2 && str1[0].equals(str2[0])) {
-                return str1[1].compareTo(str2[1]);
-            }
-
-            return Long.compare(Long.parseLong(str1[0]), Long.parseLong(str2[0]));
-        }
-    });
+    List<K> removedKeyList = new LinkedList<>();
 
     // Todo : eviction 메소드 작성
     public void eviction() {
         System.out.println("\n[Eviction start]");
-        List<K> removedKeyList = new LinkedList<>();
 
-        // 타임스탬프가 가장 오래된 entry 중 최대 크기의 일정 비율을 삭제 처리
+        // LRU 알고리즘으로 eviction 대상을 삭제 처리
         for (int i = 0; i < (MAX_SIZE / 2); i++) {
-            System.out.println(timeStampKeySet);
-            String str = timeStampKeySet.first();
-            String[] splitStr = str.split("=<-->=");
-            timeStampKeySet.remove(str);
-            K target = (K) splitStr[1];
+            System.out.println(queue); // 디버그
+            K target = queue.remove(0);
             cacheMemory.remove(target);
             removedKeyList.add(target);
-            System.out.println(timeStampKeySet);
+            System.out.println(queue); // 디버
         }
 
         System.out.println("- Removed key list -");
         System.out.println(removedKeyList);
+        removedKeyList.clear();
+        System.out.println(SERVER_CACHE_EVICTION_MSG);
     }
 
     public V put(K key, V value) {
         if (cacheMemory.size() >= MAX_SIZE) {
             System.out.println(SERVER_CACHE_FULL_MSG);
             eviction();
-            System.out.println(SERVER_CACHE_EVICTION_MSG);
-        }
 
-        // "=<-->=" 문자열은 타임스탬프와 키를 구분하기 위한 delimiter
-        timeStampKeySet.add(String.valueOf(System.nanoTime()) + "=<-->=" + key);
+        }
+        queue.add(queue.size(), key);
+
         return cacheMemory.put(key, value);
     }
 
     public V get(K key) {
+        int idx = queue.indexOf(key);
+        // Get 연산을 수행한 앤트리는 큐에 다시 넣어서 life-time을 갱신함
+        queue.add(queue.size() - 1, queue.remove(idx));
+
         return cacheMemory.get(key);
     }
 
     public V remove(K key) {
+        int idx = queue.indexOf(key);
+        queue.remove(idx);
+
         return cacheMemory.remove(key);
     }
 
